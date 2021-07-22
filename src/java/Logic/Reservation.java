781bbc24f7ea47fa2d255a05a0721136ff379a21
Jose <jmejiamu@gmail.com>
2021-07-21 17:02:32 -0700

    added more fiels to reservation

--- src/java/Logic/Reservation.java
+++ src/java/Logic/Reservation.java
@@ -4,19 +4,25 @@ package Logic;
 import java.util.Date;
 
 
-public class Reservation {
+public class Reservation{
      private int id;
-     private Date checkIn;
-     private Date checkOut;
+     private Date checkIn; //
+     private Date checkOut; //
      private boolean aviable;
      private double cost;
-     private int numOfPeople;
+     private int numOfPeople;//
      private String typeOfRoom; //Single, double, triple ..
+     private String firstName;
+     private String lastName;
+     private Date dob;
+     private String dni;
+     private String profession;
+     private String address;
 
     public Reservation() {
     }
 
-    public Reservation(int id, Date checkIn, Date checkOut, boolean aviable, double cost, int numOfPeople, String typeOfRoom) {
+    public Reservation(int id, Date checkIn, Date checkOut, boolean aviable, double cost, int numOfPeople, String typeOfRoom, String firstName, String lastName, Date dob, String dni, String profession, String address) {
         this.id = id;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
@@ -24,6 +30,12 @@ public class Reservation {
         this.cost = cost;
         this.numOfPeople = numOfPeople;
         this.typeOfRoom = typeOfRoom;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.dni = dni;
+        this.profession = profession;
+        this.address = address;
     }
 
     public int getId() {
@@ -80,6 +92,54 @@ public class Reservation {
 
     public void setTypeOfRoom(String typeOfRoom) {
         this.typeOfRoom = typeOfRoom;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
      
      
