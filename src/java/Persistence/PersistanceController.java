29755536b9f3f3b54737a388b5ab2433222a8ad7
Jose <jmejiamu@gmail.com>
2021-07-21 17:20:34 -0700

    create method that create  new reservation

--- src/java/Persistence/PersistanceController.java
+++ src/java/Persistence/PersistanceController.java
@@ -1,7 +1,16 @@
 
 package Persistence;
 
+import Logic.Reservation;
+
 
 public class PersistanceController {
     
+    ReservationJpaController reservationJpaController = new ReservationJpaController();
+    
+    public void createReservation(Reservation reservation){
+        
+        reservationJpaController.create(reservation);
+        
+    }
 }
